9610e0a1c5905bd252333210f46ea317377a302a
PAPI131099 <58227775+PAPI131099@users.noreply.github.com>
2022-03-13 14:39:07 +0100

    Finalizacion de fibonacci

--- fibonacci/Fibonacci.js
+++ fibonacci/Fibonacci.js
@@ -1,12 +1,12 @@
 export default class Fibonacci {
   constructor(id) {
     this.id = id;
+    this.text = document.getElementById("render");
     this.num = 0;
   }
   onKeyUp() {
     this.id.onkeyup = () => {
-      console.log(this.id.value);
-      console.log(`Fibonaccio: ${this.fibonacci(this.id.value)}`);
+      this.render(this.id.value);
     };
   }
   fibonacci(num) {
@@ -19,7 +19,13 @@ export default class Fibonacci {
     }
     return fibo[num];
   }
-  render() {
-    console.log(this.id);
+  render(num) {
+    let fibo = this.fibonacci(num);
+    this.text.innerText = `0, 1`;
+    let suma = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      suma[i] = suma[i - 1] + suma[i - 2];
+      this.text.innerText += `, ${suma[i]}`;
+    }
   }
 }
